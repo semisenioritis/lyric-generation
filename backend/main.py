0c070b6cf0d9841149efffbd5e4df4184ed6e8ab
semisenioritis <atharvadhamankar43@gmail.com>
2023-04-13 02:04:40 +0530

    backend sync push
    this push contains most of the required backend to solve the whole problem. i have left place holders for  ed and rihanna functions but for now this is it.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,21 @@
 from flask import Flask
-
+import modelrunner as mod
 app = Flask(__name__)
 
 #api route
 
 @app.route("/<artist>")
 def artists(artist):
-    return {"lyric" : f"{artist}"}
+    # call main function inside this
+    if artist=="Taylor Swift":
+        lyrics=mod.taylor_caller()
+    elif artist=="Ed Sheeran":
+        lyrics=mod.ed_caller()
+    elif artist=="Rihanna":
+        lyrics=mod.rihanna_caller()
+    else:
+        lyrics=''
+    return {"lyric" : f"{lyrics}"}
 
 
 if __name__ ==  "__main__":
